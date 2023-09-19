91b5233042bfcb7235e9b1166f74cf7eeb0212a7
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-09-19 13:28:48 +0700

    style: format

--- cpu.py
+++ cpu.py
@@ -16,6 +16,7 @@ from gensim.models.ldamulticore import LdaMulticore
 
 stop_words = StopWordRemoverFactory().get_stop_words()
 
+
 def get_docs(pathf, ast_parse=False):
     docs = pd.read_csv(pathf)
     docs = docs['article'].to_list()
@@ -24,28 +25,31 @@ def get_docs(pathf, ast_parse=False):
         docs = [ast.literal_eval(d) for d in docs]
     return docs
 
+
 def train_b(docs, **bargs):
     pipe = make_pipeline(
         TfidfVectorizer(),
         TruncatedSVD(100)
     )
     model = BERTopic(
-                embedding_model=pipe,
-                nr_topics='auto',
-                **bargs
-            )
+        embedding_model=pipe,
+        nr_topics='auto',
+        **bargs
+    )
     model.fit_transform(docs)
     return model
+
 
 def get_bargs(v):
     bargs = {}
     if 'p' in v:
         vectorizer_model = CountVectorizer(
-            ngram_range=(1,3),
+            ngram_range=(1, 3),
             stop_words=stop_words
         )
         bargs['vectorizer_model'] = vectorizer_model
     return bargs
+
 
 def get_coherence_b(model, docs):
     vectorizer = model.vectorizer_model
@@ -54,16 +58,18 @@ def get_coherence_b(model, docs):
     dictionary = corpora.Dictionary(tokens)
     topics = get_topics_bertopic(model, all=True)
     c = get_coherence(
-            topics=topics,
-            texts=tokens,
-            dictionary=dictionary
-            )
+        topics=topics,
+        texts=tokens,
+        dictionary=dictionary
+    )
     return c
+
 
 def timed(f):
     start = time()
     result = f()
     return result, time() - start
+
 
 def get_topic_freq_l(model, corpus):
     topic_distribution = model.get_document_topics(corpus)
@@ -75,8 +81,10 @@ def get_topic_freq_l(model, corpus):
             topic_freq[t] = topic_freq[t] + 1
     return topic_freq
 
+
 def get_top_topics_b(model):
     topics = model.get_topics()
+
 
 vs = 'ar,ap,sr,sp'.split(',')
 
@@ -84,7 +92,7 @@ path = './datasets/ds/bertopic'
 ms = {}
 cs = {}
 ds = {}
-fs = {} # frequency topics
+fs = {}  # frequency topics
 t_t = {}
 t_c = {}
 t_d = {}
@@ -94,7 +102,8 @@ for v in (tv := tqdm(vs)):
     docs = get_docs(_path)
     ms[v], t_t[v] = timed(lambda: train_b(docs, **get_bargs(v)))
     cs[v], t_c[v] = timed(lambda: get_coherence_b(ms[v], docs))
-    ds[v], t_d[v] = timed(lambda: get_diversity(get_topics_bertopic(ms[v], all=True)))
+    ds[v], t_d[v] = timed(lambda: get_diversity(
+        get_topics_bertopic(ms[v], all=True)))
 
 lpath = './datasets/ds/lda'
 lms = {}
@@ -111,19 +120,19 @@ for v in (tv := tqdm(vs)):
     corpus = [id2word.doc2bow(d) for d in docs]
     num_topics = len(ms[v].get_topics())
     lms[v], lt_t[v] = timed(lambda: LdaMulticore(
-                corpus,
-                num_topics,
-                id2word
-            ))
+        corpus,
+        num_topics,
+        id2word
+    ))
     list(lms[v].get_document_topics(corpus))
     get_topic_freq_l(lms[v], corpus)
     break
     lcs[v], lt_c[v] = timed(lambda: get_coherence(
-                model=lms[v],
-                texts=docs,
-                dictionary=id2word
-            ))
-    lds[v],lt_d[v] = timed(get_diversity(get_topics_lda(lms[v], id2word)))
+        model=lms[v],
+        texts=docs,
+        dictionary=id2word
+    ))
+    lds[v], lt_d[v] = timed(get_diversity(get_topics_lda(lms[v], id2word)))
 
 # len(ms['ar'].get_topics())
 ms['ap'].get_topic_freq()['Topic'][2]
