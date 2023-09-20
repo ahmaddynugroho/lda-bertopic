c8436289c803b0ae69e1ee867799f7667e049975
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-09-20 23:33:04 +0700

    feat: evaluation

--- cpu.py
+++ cpu.py
@@ -1,157 +1,150 @@
-import pandas as pd
-from time import time
-import os
-from bertopic import BERTopic
-from sklearn.pipeline import make_pipeline
-from sklearn.decomposition import TruncatedSVD
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-from tqdm.auto import tqdm
-from utils import get_topics_bertopic, get_coherence, get_diversity, get_topics_lda
-import gensim.corpora as corpora
-from gensim.models.coherencemodel import CoherenceModel
+# %% imports
 import ast
+import json
+from statistics import mean
+from time import time
+
+import gensim.corpora as corpora
+import pandas as pd
+import xarray as xr
+from bertopic import BERTopic
+from gensim.models.coherencemodel import CoherenceModel
 from gensim.models.ldamulticore import LdaMulticore
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+from tqdm.auto import tqdm
+
+from utils import get_coherence, get_diversity, get_topics_bertopic, get_topics_lda
 
 stop_words = StopWordRemoverFactory().get_stop_words()
 
 
+# %% get docs as list
 def get_docs(pathf, ast_parse=False):
     docs = pd.read_csv(pathf)
-    docs = docs['article'].to_list()
-    docs = docs[:300]
+    docs.columns = ["doc"]
+    docs = docs["doc"].to_list()
+    docs = docs[:100]
     if ast_parse:
         docs = [ast.literal_eval(d) for d in docs]
     return docs
 
 
+# %% train bertopic
 def train_b(docs, **bargs):
-    pipe = make_pipeline(
-        TfidfVectorizer(),
-        TruncatedSVD(100)
-    )
-    model = BERTopic(
-        embedding_model=pipe,
-        nr_topics='auto',
-        **bargs
-    )
+    pipe = make_pipeline(TfidfVectorizer(), TruncatedSVD(100))
+    model = BERTopic(embedding_model=pipe, nr_topics="auto", **bargs)
     model.fit_transform(docs)
     return model
 
 
+# %% get bertopic training args
 def get_bargs(v):
     bargs = {}
-    if 'p' in v:
-        vectorizer_model = CountVectorizer(
-            ngram_range=(1, 3),
-            stop_words=stop_words
-        )
-        bargs['vectorizer_model'] = vectorizer_model
+    if "wg" in v:
+        vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words=stop_words)
+        bargs["vectorizer_model"] = vectorizer_model
     return bargs
 
 
+# %% get bertopic coherence
 def get_coherence_b(model, docs):
     vectorizer = model.vectorizer_model
     analyzer = vectorizer.build_analyzer()
     tokens = [analyzer(doc) for doc in docs]
     dictionary = corpora.Dictionary(tokens)
     topics = get_topics_bertopic(model, all=True)
-    c = get_coherence(
-        topics=topics,
-        texts=tokens,
-        dictionary=dictionary
-    )
+    c = get_coherence(topics=topics, texts=tokens, dictionary=dictionary)
     return c
 
 
+# %% used to get training time and basically any lambda fn
 def timed(f):
     start = time()
     result = f()
     return result, time() - start
 
 
-def get_topic_freq_l(model, corpus):
-    topic_distribution = model.get_document_topics(corpus)
-    topic_freq = {}
-    for tf in topic_distribution:
-        for t, _ in tf:
-            if t not in topic_freq:
-                topic_freq[t] = 0
-            topic_freq[t] = topic_freq[t] + 1
-    return topic_freq
+# %% variants
+vs = "ar,awg,alwg,sr,swg,slwg".split(",")
 
-
-def get_top_topics_b(model):
-    topics = model.get_topics()
-
-
-vs = 'ar,ap,sr,sp'.split(',')
-
-path = './datasets/ds/bertopic'
-ms = {}
-cs = {}
+# %% bertopic training
+path = "./datasets/dsn/"
 ds = {}
-fs = {}  # frequency topics
-t_t = {}
-t_c = {}
-t_d = {}
 for v in (tv := tqdm(vs)):
-    tv.set_description('bertopic')
-    _path = f'{path}/{v}.csv'
+    bv = f"b_{v}"
+    tv.set_description(f"bertopic {bv}")
+    _path = f"{path}/{bv}.csv"
     docs = get_docs(_path)
-    ms[v], t_t[v] = timed(lambda: train_b(docs, **get_bargs(v)))
-    cs[v], t_c[v] = timed(lambda: get_coherence_b(ms[v], docs))
-    ds[v], t_d[v] = timed(lambda: get_diversity(
-        get_topics_bertopic(ms[v], all=True)))
+    r = {
+        "c": [],
+        "d": [],
+        "tt": [],
+        "tc": [],
+        "td": [],
+        "t": [],
+        "s": [],
+    }
+    for i in range(3):
+        m, tt = timed(lambda: train_b(docs, **get_bargs(bv)))
+        c, tc = timed(lambda: get_coherence_b(m, docs))
+        d, td = timed(lambda: get_diversity(get_topics_bertopic(m, all=True)))
+        t = tt + tc + td
+        s = c * d
+        r["c"].append(c)
+        r["d"].append(d)
+        r["t"].append(t)
+        r["s"].append(s)
+        r["tt"].append(tt)
+        r["tc"].append(tc)
+        r["td"].append(td)
+        if all(s >= x for x in r["s"]):
+            bt = json.dumps(get_topics_bertopic(m, all=True))
+    r = {k: mean(v) for (k, v) in r.items() if not k is "bt"}
+    r["tw"] = bt
+    ds[bv] = r
 
-lpath = './datasets/ds/lda'
-lms = {}
-lcs = {}
-lds = {}
-lt_t = {}
-lt_c = {}
-lt_d = {}
+
+# %% bertopic training
 for v in (tv := tqdm(vs)):
-    tv.set_description('lda')
-    _path = f'{lpath}/{v}.csv'
+    lv = f"l_{v}"
+    tv.set_description(f"lda {v}")
+    _path = f"{path}/{lv}.csv"
     docs = get_docs(_path, ast_parse=True)
     id2word = corpora.Dictionary(docs)
     corpus = [id2word.doc2bow(d) for d in docs]
-    num_topics = len(ms[v].get_topics())
-    lms[v], lt_t[v] = timed(lambda: LdaMulticore(
-        corpus,
-        num_topics,
-        id2word
-    ))
-    list(lms[v].get_document_topics(corpus))
-    get_topic_freq_l(lms[v], corpus)
-    break
-    lcs[v], lt_c[v] = timed(lambda: get_coherence(
-        model=lms[v],
-        texts=docs,
-        dictionary=id2word
-    ))
-    lds[v], lt_d[v] = timed(get_diversity(get_topics_lda(lms[v], id2word)))
+    num_topics = len(json.loads(ds[f"b_{v}"]["tw"]))
+    r = {
+        "c": [],
+        "d": [],
+        "tt": [],
+        "tc": [],
+        "td": [],
+        "t": [],
+        "s": [],
+    }
+    for i in range(3):
+        m, tt = timed(lambda: LdaMulticore(corpus, num_topics, id2word))
+        c, tc = timed(lambda: get_coherence(model=m, texts=docs, dictionary=id2word))
+        d, td = timed(lambda: get_diversity(get_topics_lda(m, id2word)))
+        t = tt + tc + td
+        s = c * d
+        r["c"].append(c)
+        r["d"].append(d)
+        r["t"].append(t)
+        r["s"].append(s)
+        r["tt"].append(tt)
+        r["tc"].append(tc)
+        r["td"].append(td)
+        if all(s >= x for x in r["s"]):
+            lt = json.dumps(get_topics_lda(m, id2word))
+    r = {k: mean(v) for (k, v) in r.items()}
+    r["tw"] = lt
+    ds[lv] = r
 
-# len(ms['ar'].get_topics())
-ms['ap'].get_topic_freq()['Topic'][2]
-ms['ap'].get_topic_freq()['Count']
-ms['ap'].get_topic_freq(len(ms['ap'].get_topics()) - 1)
-ms['ar'].get_topics()
-ms['ar'].get_topic_freq().reset_index(drop=True)
-ms['ap'].get_topics()
 
-ms
-cs
-ds
-t_t
-t_c
-t_d
-
-lms
-lcs
-lds
-lt_t
-lt_c
-lt_d
+# %% save ds
+ds_df = pd.DataFrame.from_dict(ds, orient="index")
+ds_df.to_csv("./results/haruna.csv")
