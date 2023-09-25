202395fdc2df087cbeeb1f829c3fa8f0cb16018f
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-09-25 11:28:28 +0700

    style: format

--- scrape/scrape/spiders/__init__.py
+++ scrape/scrape/spiders/__init__.py
@@ -6,23 +6,22 @@ import scrapy
 
 
 class Tempo(scrapy.Spider):
-    name = 'tempo'
+    name = "tempo"
     start_urls = [
-        f'https://www.tempo.co/indeks/2022-12-{1 + day:02}' for day in range(31)
+        f"https://www.tempo.co/indeks/2022-12-{1 + day:02}" for day in range(31)
     ]
 
     def parse(self, response):
-        links = response.css('.title a::attr(href)').getall()
+        links = response.css(".title a::attr(href)").getall()
         for a in links:
             yield scrapy.Request(a, callback=self.parse_article)
 
     def parse_article(self, response):
-        headline = response.css('.title::text').get().strip()
-        body = response.css('.detail-konten>p *::text').getall()
-        body = [p.replace('\xa0', ' ').strip() for p in body]
-        body = ' '.join(body)
+        headline = response.css(".title::text").get().strip()
+        body = response.css(".detail-konten>p *::text").getall()
+        body = [p.replace("\xa0", " ").strip() for p in body]
+        body = " ".join(body)
         if headline and body:
-            yield {
-                'headline': headline,
-                'body': body
-            }
+            yield {"headline": headline, "body": body}
+
+
