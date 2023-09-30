7fe834a706702e37c57bdacd61f7c2ebc34ee2ac
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-10-01 06:11:45 +0700

    style: move functions to lib

--- cpu.py
+++ cpu.py
@@ -1,97 +1,15 @@
 # %% imports
-import ast
 import json
 from statistics import mean
-from time import time
 
 import gensim.corpora as corpora
 import pandas as pd
-from bertopic import BERTopic
 from gensim.models.ldamulticore import LdaMulticore
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-from sklearn.decomposition import TruncatedSVD
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.pipeline import make_pipeline
 from tqdm.auto import tqdm
 
-from utils import get_coherence, get_diversity, get_topics_bertopic, get_topics_lda
-
-
-# %% get docs as list
-def get_docs(pathf, ast_parse=False):
-    docs = pd.read_csv(pathf)
-    docs.columns = ["doc"]
-    docs = docs["doc"].to_list()
-    docs = docs[:100]
-    if ast_parse:
-        docs = [ast.literal_eval(d) for d in docs]
-    return docs
-
-
-# %% train bertopic
-def train_b(docs, use_gpu=False, **bargs):
-    if use_gpu:
-        model = BERTopic(language="multilingual", nr_topics="auto", **bargs)
-    else:
-        pipe = make_pipeline(TfidfVectorizer(), TruncatedSVD(100))
-        model = BERTopic(embedding_model=pipe, nr_topics="auto", **bargs)
-    topics, prob = model.fit_transform(docs)
-    return (model, topics)
-
-
-# %% get bertopic training args
-def get_bargs(v):
-    bargs = {}
-    if "wg" in v:
-        stop_words = StopWordRemoverFactory().get_stop_words()
-        vectorizer_model = CountVectorizer(ngram_range=(1, 3), stop_words=stop_words)
-        bargs["vectorizer_model"] = vectorizer_model
-    return bargs
-
-
-# %% get bertopic coherence
-def get_coherence_b(m, topics, docs):
-    vectorizer = m.vectorizer_model
-    analyzer = vectorizer.build_analyzer()
-    tokens = [analyzer(doc) for doc in docs]
-    dictionary = corpora.Dictionary(tokens)
-    topic_words = get_topics_bertopic(m, topics, dictionary)
-    k = len(topic_words)
-    c = get_coherence(topics=topic_words, texts=tokens, dictionary=dictionary)
-    return (c, k, topic_words)
-
-
-# %% used to get training time and basically any lambda fn
-def timed(f):
-    start = time()
-    result = f()
-    return result, time() - start
-
-
-# %% get top 5 most used topics in bertopic
-def get_top_7_b(model):
-    r = []
-    topics = model.get_topic_freq().head(7)["Topic"]
-    for t in topics:
-        tw = model.get_topic(t)
-        r.append([w for w, _ in tw])
-    return r
-
-
-# %% get top 5 most used topics in lda
-def get_top_7_l(m, corpus):
-    td = {"Count": {}}
-    for bow in corpus:
-        _tds = sorted(m[bow], key=lambda x: x[1], reverse=True)
-        ti = _tds[0][0]
-        td["Count"][ti] = td["Count"][ti] + 1 if ti in td["Count"] else 1
-    td = pd.DataFrame.from_dict(td).sort_values(by="Count", ascending=False).head(7)
-    _tw = []
-    for ti in td.index:
-        ti
-        _tw.append([w for w, p in m.show_topic(ti)])
-    return _tw
-
+from lib import (get_docs, train_b, get_coherence_b, get_diversity, timed,
+                 get_bargs, get_top_7_b, get_top_7_l, get_coherence,
+                 get_topics_lda)
 
 # %% variants
 vs = "ar,awg,alwg,sr,swg,slwg".split(",")
@@ -143,7 +61,6 @@ for v in (tv := tqdm(vs, position=0)):
     r["kt"] = kt
     ds[bv] = r
 
-
 # %% bertopic training GPU
 for v in (tv := tqdm(vs, position=0)):
     bv = f"b_{v}"
@@ -189,7 +106,6 @@ for v in (tv := tqdm(vs, position=0)):
     r["kt"] = kt
     ds[bgv] = r
 
-
 # %% lda training
 for v in (tv := tqdm(vs, position=0)):
     lv = f"l_{v}"
@@ -212,7 +128,8 @@ for v in (tv := tqdm(vs, position=0)):
     for i in tqdm(range(5), desc="runs", position=1, leave=False):
         num_topics = json.loads(ds_runs[f"b_{v}"]["k"])[i]
         m, tt = timed(lambda: LdaMulticore(corpus, num_topics, id2word))
-        c, tc = timed(lambda: get_coherence(model=m, texts=docs, dictionary=id2word))
+        c, tc = timed(
+            lambda: get_coherence(model=m, texts=docs, dictionary=id2word))
         d, td = timed(lambda: get_diversity(get_topics_lda(m, id2word)))
         t = tt + tc + td
         s = c * d
@@ -234,7 +151,6 @@ for v in (tv := tqdm(vs, position=0)):
     r["t5t"] = t5t
     r["kt"] = kt
     ds[lv] = r
-
 
 # %% save ds
 ds_df = pd.DataFrame.from_dict(ds, orient="index")
--- lib.py
+++ lib.py
@@ -1,12 +1,26 @@
-import pickle
-from gensim.models.coherencemodel import CoherenceModel
-from bertopic import BERTopic
+import ast
+from time import time
+
+import gensim.corpora as corpora
 import pandas as pd
+from bertopic import BERTopic
+from gensim.models.coherencemodel import CoherenceModel
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import (
+    StopWordRemoverFactory)
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import make_pipeline
+
 
 def get_coherence(topics=None, model=None, texts=None, dictionary=None):
-    c = CoherenceModel(model=model, topics=topics, texts=texts, dictionary=dictionary, coherence='c_v')
+    c = CoherenceModel(model=model,
+                       topics=topics,
+                       texts=texts,
+                       dictionary=dictionary,
+                       coherence='c_v')
     cs = c.get_coherence()
     return cs
+
 
 def get_diversity(topics):
     # https://github.com/MIND-Lab/OCTIS/blob/02fff36346b72818990b9d4d636a7bfb13aa048b/octis/evaluation_metrics/diversity_metrics.py#L12
@@ -17,6 +31,7 @@ def get_diversity(topics):
     td = len(unique_words) / (topk * len(topics))
     return td
 
+
 def get_topics_lda(lda, dictionary):
     k = lda.num_topics
     topics = []
@@ -26,6 +41,7 @@ def get_topics_lda(lda, dictionary):
         topics.append(ki)
     return topics
 
+
 def get_topics_bertopic(m, topics, dictionary):
     topic_words = []
     for topic in range(len(set(topics)) - m._outliers):
@@ -34,3 +50,81 @@ def get_topics_bertopic(m, topics, dictionary):
         topic_words.append(words)
     topic_words = [words for words in topic_words if len(words) > 0]
     return topic_words
+
+
+# %% get docs as list
+def get_docs(pathf, ast_parse=False):
+    docs = pd.read_csv(pathf)
+    docs.columns = ["doc"]
+    docs = docs["doc"].to_list()
+    docs = docs[:100]
+    if ast_parse:
+        docs = [ast.literal_eval(d) for d in docs]
+    return docs
+
+
+# %% train bertopic
+def train_b(docs, use_gpu=False, **bargs):
+    if use_gpu:
+        model = BERTopic(language="multilingual", nr_topics="auto", **bargs)
+    else:
+        pipe = make_pipeline(TfidfVectorizer(), TruncatedSVD(100))
+        model = BERTopic(embedding_model=pipe, nr_topics="auto", **bargs)
+    topics, prob = model.fit_transform(docs)
+    return (model, topics)
+
+
+# %% get bertopic training args
+def get_bargs(v):
+    bargs = {}
+    if "wg" in v:
+        stop_words = StopWordRemoverFactory().get_stop_words()
+        vectorizer_model = CountVectorizer(ngram_range=(1, 3),
+                                           stop_words=stop_words)
+        bargs["vectorizer_model"] = vectorizer_model
+    return bargs
+
+
+# %% get bertopic coherence
+def get_coherence_b(m, topics, docs):
+    vectorizer = m.vectorizer_model
+    analyzer = vectorizer.build_analyzer()
+    tokens = [analyzer(doc) for doc in docs]
+    dictionary = corpora.Dictionary(tokens)
+    topic_words = get_topics_bertopic(m, topics, dictionary)
+    k = len(topic_words)
+    c = get_coherence(topics=topic_words, texts=tokens, dictionary=dictionary)
+    return (c, k, topic_words)
+
+
+# %% used to get training time and basically any lambda fn
+def timed(f):
+    start = time()
+    result = f()
+    return result, time() - start
+
+
+# %% get top 5 most used topics in bertopic
+def get_top_7_b(model):
+    r = []
+    topics = model.get_topic_freq().head(7)["Topic"]
+    for t in topics:
+        tw = model.get_topic(t)
+        r.append([w for w, _ in tw])
+    return r
+
+
+# %% get top 5 most used topics in lda
+def get_top_7_l(m, corpus):
+    td = {"Count": {}}
+    for bow in corpus:
+        _tds = sorted(m[bow], key=lambda x: x[1], reverse=True)
+        ti = _tds[0][0]
+        td["Count"][ti] = td["Count"][ti] + 1 if ti in td["Count"] else 1
+    td = pd.DataFrame.from_dict(td).sort_values(by="Count",
+                                                ascending=False).head(7)
+    _tw = []
+    for ti in td.index:
+        ti
+        _tw.append([w for w, p in m.show_topic(ti)])
+    return _tw
